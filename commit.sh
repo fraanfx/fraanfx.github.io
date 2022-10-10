30bb4afb19e4d234e70087ff5e8a24aa01e279e4
fraanfx <fran.am95@gmail.com>
2022-10-10 20:30:08 +0200

    Update, keep wroking

---
+++ commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash">bash
+
+read -p "enter the . or files to commit: " files
+git add .
+read -P "enter your comment here: " comment
+echo "comment"
+git commit -m (comment)
+read -p "quieres cambiar el origen del comment: " originfiles
+   if (originfiles) {
+      git remote add 
+   }
+read -p "Quieres pushear: " pushear
+if (pushear == si){
+  git push
+}
+clear
+echo "Finished"
+     
+   
--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
+const textDisplay = document.getElementById('animated-text');
+const phrases = ['UX Designer ', 'FrontEnd Developer ', 'UX Engineer '];
+let iP = 0;
+let jP = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
+const loopPhrase  = () =>{
+	isEnd = false;
+	textDisplay.innerHTML = currentPhrase.join('');
+
+	if(iP < phrases.length){
+	
+		if(!isDeleting && jP <= phrases[iP].length){
+			currentPhrase.push(phrases[iP][jP])
+			jP++;
+			textDisplay.innerHTML = currentPhrase.join('');
+		}
+		if(isDeleting && jP <= phrases[iP].length){
+			currentPhrase.pop(phrases[iP][jP]);
+			jP--;
+			textDisplay.innerHTML = currentPhrase.join('');
+		}
+		if (jP == phrases[iP].length){
+			isEnd = true;
+			isDeleting = true;
+		}
+		if(isDeleting && jP === 0) {
+			currentPhrase = [];
+			isDeleting = false;
+			iP++;
+			if(iP == phrases.length ){
+				iP = 0;
+			}
+		}
+	}
+	const speedUp = Math.random() * (80 - 50) + 80;
+		const normalSpeed = Math.random() * (300 - 200) +200;
+		const time = isEnd ? 2000 : isDeleting ? speedUp : normalSpeed;
+	setTimeout(loopPhrase, time)
+}
+loopPhrase();
 $(document).ready(function () {
 	$('.menu-toggler').on('click', function () {
 		$(this).toggleClass('open');
