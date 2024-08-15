9d07cc10adbcac0dc353bdb128c8fe694e9f9f0c
fraanfx <fran.am95@gmail.com>
2024-08-15 02:36:48 +0200

    metadata + logos + anim

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 
 read -p "enter the . or files to commit: " files
 git add .
-read -P "enter your comment here: " comment
+read -P "enter your comment here: " (comment)
 echo "comment"
 git commit -m (comment)
 read -p "quieres cambiar el origen del comment: " originfiles
